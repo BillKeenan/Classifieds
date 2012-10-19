37f9088aa71ef092f3bd19776fb5e0eb8ebb0460
Bill Keenan <bill@creatived.com>
2012-10-19 12:01:11 -0400

    Project running with custom membership provider, now with files!

---
+++ src/Classifieds/Classifieds/Filters/InitializeSimpleMembershipAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Web.Mvc;
+using Classifieds.services.UserServices;
+using WebMatrix.WebData;
+using Classifieds.Models;
+
+namespace Classifieds.Filters
+{
+        public class AuthorizationAttribute : ActionFilterAttribute, IActionFilter
+        {
+            public MyRole UserRole { get; set; }
+
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Ensure ASP.NET Simple Membership is initialized only once per app start
+        }
+
+        
+    }
+}
---
+++ src/Classifieds/Classifieds/services/UserServices/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Classifieds.services.UserServices
+{
+    public class MyRole
+    {
+    }
+}
